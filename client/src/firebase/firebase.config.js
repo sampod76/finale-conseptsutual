0ed9064058dab5e3999173cd937aef6748307952
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-23 14:10:27 +0600

    first commit

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+//           **finale consept session firebase 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRY1Y7fHLcCNdnkQD2kx4NXXPYcZZHNoA",
+  authDomain: "finale-consept-session.firebaseapp.com",
+  projectId: "finale-consept-session",
+  storageBucket: "finale-consept-session.appspot.com",
+  messagingSenderId: "929614834086",
+  appId: "1:929614834086:web:04af310a8ff0f6ddbc8767"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
